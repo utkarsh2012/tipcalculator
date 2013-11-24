ecc640ea44d4976c45e7cf64d51d0873d5179768
Utkarsh Sengar <utkarsh2012@gmail.com>
2013-11-24 01:30:59 -0800

    Initial Commit

---
+++ tipcalculator/IOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IOSAppDelegate.h
+//  tipcalculator
+//
+//  Created by Utkarsh Sengar on 11/24/13.
+//  Copyright (c) 2013 area42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IOSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Utkarsh Sengar on 11/24/13.
+//  Copyright (c) 2013 area42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IOSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IOSAppDelegate class]));
+    }
+}
