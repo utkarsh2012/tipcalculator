7ce3dd387abd8c72ca4214c639cd8a0ffb00c4a3
Utkarsh Sengar <utkarsh2012@gmail.com>
2013-11-24 23:59:19 -0800

    Completed walkthrough, let the real work begin..

--- tipcalculator/TipViewController.m
+++ tipcalculator/TipViewController.m
@@ -9,7 +9,12 @@
 #import "TipViewController.h"
 
 @interface TipViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+- (IBAction)onTap:(id)sender;
+- (void)updateValues;
 @end
 
 @implementation TipViewController
@@ -27,7 +32,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self updateValues];
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,4 +41,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self updateValues];
+}
+
+- (void)updateValues {
+    float billAmount = [self.billTextField.text floatValue];
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
+    float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    float totalAmount = tipAmount + billAmount;
+    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+}
 @end
