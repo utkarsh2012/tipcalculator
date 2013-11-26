9860e8d35bdf49f26ff318859f85a59a2077e2e9
Utkarsh Sengar <utkarsh2012@gmail.com>
2013-11-26 00:16:44 -0800

    Hacked the shit out of it to make it uber simple

--- tipcalculator/TipViewController.m
+++ tipcalculator/TipViewController.m
@@ -9,22 +9,29 @@
 #import "TipViewController.h"
 
 @interface TipViewController ()
-@property (weak, nonatomic) IBOutlet UITextField *billTextField;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+@property (weak, nonatomic) IBOutlet UILabel *billLabel;
 - (IBAction)onTap:(id)sender;
 - (void)updateValues;
 @end
 
 @implementation TipViewController
 
+static NSString *billTextField;
+
++(void)initialize {
+    if (self == [TipViewController class]) {
+        billTextField = @"0";
+    }
+}
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        self.title = @"Tip Calculator";
-        // Custom initialization
+        self.title = @"Simple Tip";
     }
     return self;
 }
@@ -46,12 +53,42 @@
     [self updateValues];
 }
 
+- (IBAction)onTapFromTipControl:(id)sender {
+    [self computeValues:[billTextField floatValue]];
+}
+
 - (void)updateValues {
-    float billAmount = [self.billTextField.text floatValue];
+    [self renderAlert];
+}
+
+- (void) renderAlert {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Bill" message:@"What is your bill?" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok", nil];
+
+    alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+    UITextField *textField = [alertView textFieldAtIndex:0];
+    textField.keyboardType = UIKeyboardTypeNumberPad;
+    
+    [alertView show];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex)
+    {
+        billTextField = [[alertView textFieldAtIndex:0] text];
+        float billAmount = [billTextField floatValue];
+        [self computeValues:billAmount];
+    }
+}
+
+- (void)computeValues:(float)billAmount{
     NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
     float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
     float totalAmount = tipAmount + billAmount;
-    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
-    self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+    
+    self.billLabel.text = [NSMutableString stringWithFormat:@"$%0.2f", billAmount];
+    self.tipLabel.text = [NSMutableString stringWithFormat:@"+ $%0.2f", tipAmount];
+    self.totalLabel.text = [NSMutableString stringWithFormat:@"$%0.2f", totalAmount];
 }
+
 @end
